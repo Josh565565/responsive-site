b89b4823722063cfaa7e6dcb14ec99389b600fc7
Okwor Joshua <105440696+Josh565565@users.noreply.github.com>
2022-06-20 22:40:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
+document.getElementById("submit").addEventListener("click", function (event) {
+  event.preventDefault();
 
-document.getElementById("submit").addEventListener("click",function(event){
-    event.preventDefault();
-
-    checkData();
+  checkData();
 });
 
 var firstname = document.getElementById("firstname");
@@ -10,64 +9,57 @@ var lastname = document.getElementById("lastname");
 var email = document.getElementById("email");
 var password = document.getElementById("password");
 
-function checkData(){
-    var firstnameValue = firstname.value.trim();
-    var lastnameValue = lastname.value.trim();
-    var emailValue = email.value.trim();
-    var passwordValue = password.value.trim();
-    
-    if(firstnameValue == ""){
-        setError(firstname,"First Name cannot be empty");
-    }else{
-        setSuccess(firstname);
-    }
+function checkData() {
+  var firstnameValue = firstname.value.trim();
+  var lastnameValue = lastname.value.trim();
+  var emailValue = email.value.trim();
+  var passwordValue = password.value.trim();
 
-    if(lastnameValue == ""){
-        setError(lastname,"Last Name cannot be empty");
-    }else{
-        setSuccess(lastname);
-    }
+  if (firstnameValue == "") {
+    setError(firstname, "First Name cannot be empty");
+  } else {
+    setSuccess(firstname);
+  }
 
+  if (lastnameValue == "") {
+    setError(lastname, "Last Name cannot be empty");
+  } else {
+    setSuccess(lastname);
+  }
 
-    if(emailValue == ""){
-        setError(email,"Email cannot be empty");
-    }
-    else if(!isEmail(emailValue)){
-        setError(email,"Looks like this is not an email");
-    }
-    else{
-        setSuccess(email);
-    }
+  if (emailValue == "") {
+    setError(email, "Email cannot be empty");
+  } else if (!isEmail(emailValue)) {
+    setError(email, "Looks like this is not an email");
+  } else {
+    setSuccess(email);
+  }
 
-    if(passwordValue == ""){
-        setError(password,"Password cannot be empty");
-    }else{
-        setSuccess(password);
-    }
-
-
-
-
+  if (passwordValue == "") {
+    setError(password, "Password cannot be empty");
+  } else {
+    setSuccess(password);
+  }
 }
 
-
-function setError(u, msg){
-    var parentBox = u.parentElement;
-    parentBox.className="form-control error";
-var span = parentBox.querySelector("span");
-var fa = parentBox.querySelector(".fa");
-span.innerText=msg;
-fa.className="fa fa-exclamation-circle";
+function setError(u, msg) {
+  var parentBox = u.parentElement;
+  parentBox.className = "form-control error";
+  var span = parentBox.querySelector("span");
+  var fa = parentBox.querySelector(".fa");
+  span.innerText = msg;
+  fa.className = "fa fa-exclamation-circle";
 }
 
-function setSuccess(u){
-    var parentBox = u.parentElement;
-    parentBox.className="form-control success";
-    var fa = parentBox.querySelector(".fa");
-    fa.className="fa fa-check-circle";
+function setSuccess(u) {
+  var parentBox = u.parentElement;
+  parentBox.className = "form-control success";
+  var fa = parentBox.querySelector(".fa");
+  fa.className = "fa fa-check-circle";
 }
 
-function isEmail(e){
-    var reg = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return reg.test(e);
+function isEmail(e) {
+  var reg =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return reg.test(e);
 }
